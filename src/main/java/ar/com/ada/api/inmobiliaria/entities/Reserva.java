c015c7eee567c620e4bfea39ab3ebfde83e4d3cf
MarianellaGL <marianella.gomezluna@gmail.com>
2019-10-11 16:28:33 -0300

    gitignore con app properties y reserva

--- src/main/java/ar/com/ada/api/inmobiliaria/entities/Reserva.java
+++ src/main/java/ar/com/ada/api/inmobiliaria/entities/Reserva.java
@@ -1,11 +1,30 @@
 package ar.com.ada.api.inmobiliaria.entities;
 
+import java.util.Date;
+
+import javax.persistence.*;
+
 import ar.com.ada.api.inmobiliaria.interfaces.IOperable;
 
 /**
  * Reserva
  */
+@Entity
+@Table(name = "reserva")
 public class Reserva implements IOperable{
+
+    @Id
+    @Column(name = "reserva_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Date fecha;
+    @OneToOne(mappedBy = "locatario", cascade = CascadeType.ALL)
+    private Locatario locatarioId;
+    @OneToOne(mappedBy = "inmueble", cascade = CascadeType.ALL)
+    private Inmueble inmuebleId;
+
+
+
+
 
     
 }
