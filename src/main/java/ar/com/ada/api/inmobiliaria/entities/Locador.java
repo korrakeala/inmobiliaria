d9ef17918a980028b08aad518f3ed632fec9628e
korrakeala <renata.mansilla@gmail.com>
2019-10-11 17:14:31 -0300

    agrego annotations y otras cositas

--- src/main/java/ar/com/ada/api/inmobiliaria/entities/Locador.java
+++ src/main/java/ar/com/ada/api/inmobiliaria/entities/Locador.java
@@ -2,11 +2,34 @@ package ar.com.ada.api.inmobiliaria.entities;
 
 import java.util.*;
 
+import javax.persistence.*;
+
 /**
  * Locador
  */
+@Entity
+@Table (name = "locador")
 public class Locador extends Persona {
 
+    @Id
+    @Column(name="locador_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int locadorId;
     List<Inmueble> inmuebles = new ArrayList<Inmueble>();
+
+    public int getLocadorId() {
+        return locadorId;
+    }
+
+    public void setLocadorId(int locadorId) {
+        this.locadorId = locadorId;
+    }
+
+    public List<Inmueble> getInmuebles() {
+        return inmuebles;
+    }
+
+    public void setInmuebles(List<Inmueble> inmuebles) {
+        this.inmuebles = inmuebles;
+    }
 }
--- src/main/java/ar/com/ada/api/inmobiliaria/entities/Locatario.java
+++ src/main/java/ar/com/ada/api/inmobiliaria/entities/Locatario.java
@@ -2,12 +2,43 @@ package ar.com.ada.api.inmobiliaria.entities;
 
 import java.util.*;
 
+import javax.persistence.*;
+
 /**
  * Locatario
  */
+@Entity
+@Table (name = "locatario")
 public class Locatario extends Persona {
 
+    @Id
+    @Column(name="locatario_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int locatarioId;
     List<Reserva> reservas = new ArrayList<Reserva>();
     Usuario usuario;
+
+    public int getLocatarioId() {
+        return locatarioId;
+    }
+
+    public void setLocatarioId(int locatarioId) {
+        this.locatarioId = locatarioId;
+    }
+
+    public List<Reserva> getReservas() {
+        return reservas;
+    }
+
+    public void setReservas(List<Reserva> reservas) {
+        this.reservas = reservas;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
