47c4875b818bb6a89d2a911a3dab3309e349cda3
ecaroa <elianacarolinaa@gmail.com>
2019-10-11 15:31:06 -0300

    clase aviso

---
+++ src/main/java/ar/com/ada/api/inmobiliaria/entities/Aviso.java
@@ -0,0 +1,25 @@
+package ar.com.ada.api.inmobiliaria.entities;
+
+import javax.persistence.*;
+
+/**
+ * Aviso
+ */
+@Entity
+@Table (name ="aviso")
+public class Aviso {
+
+    @Id
+    @Column(name="aviso_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int avisoId;
+    
+    @Column(name="inmueble_id")
+    private int inmuebleId;
+
+    @Column (name="operacion_id")
+    private int operacionId
+
+
+    
+}
