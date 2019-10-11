05ddb0730952d55c246f5d45a1055c9454e6d2f1
korrakeala <renata.mansilla@gmail.com>
2019-10-11 16:28:31 -0300

    dale gitignore

--- src/main/java/ar/com/ada/api/inmobiliaria/entities/Inmobiliaria.java
+++ src/main/java/ar/com/ada/api/inmobiliaria/entities/Inmobiliaria.java
@@ -1,8 +1,6 @@
 package ar.com.ada.api.inmobiliaria.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * Inmobiliaria
