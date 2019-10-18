4e510fc3a3fe6ab3164a89adc7e376945af00c89
ecaroa <elianacarolinaa@gmail.com>
2019-10-18 13:26:57 -0300

    Crear inmueble en InmuebleRepository

--- src/main/java/ar/com/ada/api/inmobiliaria/services/InmuebleService.java
+++ src/main/java/ar/com/ada/api/inmobiliaria/services/InmuebleService.java
@@ -3,6 +3,7 @@ package ar.com.ada.api.inmobiliaria.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ar.com.ada.api.inmobiliaria.entities.Inmueble;
 import ar.com.ada.api.inmobiliaria.repo.InmuebleRepository;
 
 /**
@@ -12,6 +13,28 @@ import ar.com.ada.api.inmobiliaria.repo.InmuebleRepository;
 public class InmuebleService {
     @Autowired 
     InmuebleRepository inmuebleRepo;
+
+    public void CrearInmueble(String tipoInmueble, int cantAmb, String direccion, int superficie, int cantDormitorio, 
+    boolean aptoProf, String disposicion, int cantBanios, int antiguedadAnios){
+
+        Inmueble inmueble = new Inmueble ();
+        inmueble.setTipoInmueble(tipoInmueble);
+        inmueble.setCantAmb(cantAmb);
+        inmueble.setDireccion(direccion);
+        inmueble.setSuperficie(superficie);
+        inmueble.setCantDormitorio(cantDormitorio);
+        inmueble.setAptoProf(aptoProf);
+        inmueble.setDisposicion(disposicion);
+        inmueble.setCantBanios(cantBanios);
+        inmueble.setAntiguedadAnios(antiguedadAnios);
+        
+        //falta agregar union con usuario
+
+        inmuebleRepo.save(inmueble);
+        return inmueble;
+
+    }
+
 
     
 }
