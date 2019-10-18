f2b0fc07d79cf1dcb93ade17860d304021dd73e0
ecaroa <elianacarolinaa@gmail.com>
2019-10-18 13:00:31 -0300

    Response and Request de inmueble

---
+++ src/main/java/ar/com/ada/api/inmobiliaria/models/request/InmuebleRequest.java
@@ -0,0 +1,17 @@
+package ar.com.ada.api.inmobiliaria.models.request;
+
+/**
+ * InmuebleRequest
+ */
+public class InmuebleRequest {
+
+    public String tipoInmueble;
+    public int cantAmb;
+    public String direccion;
+    public int superficie;
+    public int cantDormitorio;
+    public boolean aptoProf;
+    public String disposicion;
+    public int cantBanios;
+    public int antiguedadAnios;
+}
