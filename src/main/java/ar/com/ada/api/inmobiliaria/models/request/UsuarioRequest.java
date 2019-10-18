cf1a088aa9701f867516ad0eaabc406b24087f7e
ecaroa <elianacarolinaa@gmail.com>
2019-10-18 12:50:20 -0300

    Creacion UsuarioRequest

---
+++ src/main/java/ar/com/ada/api/inmobiliaria/models/request/UsuarioRequest.java
@@ -0,0 +1,14 @@
+package ar.com.ada.api.inmobiliaria.models.request;
+
+import java.math.BigDecimal;
+
+/**
+ * UsuarioRequest
+ */
+public class UsuarioRequest {
+
+    public String userName;
+    public String password;
+    public String email;
+
+}
