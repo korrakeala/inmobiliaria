34f0aeaf992417635e39395136ab2601c5ac2410
MarianellaGL <marianella.gomezluna@gmail.com>
2019-10-22 00:44:59 -0300

    GETaviso funcionando

--- src/main/java/ar/com/ada/api/inmobiliaria/controllers/AvisoController.java
+++ src/main/java/ar/com/ada/api/inmobiliaria/controllers/AvisoController.java
@@ -1,12 +1,48 @@
 package ar.com.ada.api.inmobiliaria.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.inmobiliaria.entities.Aviso;
+import ar.com.ada.api.inmobiliaria.entities.Inmueble;
+import ar.com.ada.api.inmobiliaria.services.AlquilerService;
+import ar.com.ada.api.inmobiliaria.services.AvisoService;
+import ar.com.ada.api.inmobiliaria.services.VentaService;
 
 /**
  * AvisoController
  */
 @RestController
 public class AvisoController {
+
+    @Autowired
+    AvisoService as;
+    @Autowired
+    AlquilerService als;
+    @Autowired 
+    VentaService vs;
+
+
+    @GetMapping("/avisos")
+     public List<Aviso> getAvisos() {
+        List<Aviso> av = as.listarAvisos();
+
+        return av;
+     }
+
+    @GetMapping("/avisos/{id}")
+    public Aviso getAvisoById(@PathVariable int id)
+    {
+        Aviso av = as.buscarPorId(id);
+        
+        return av;
+    }
+
+    
 
     
 }
