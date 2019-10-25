afd0d1a7ad42b150e2f22e63d775f53c487896d3
MarianellaGL <marianella.gomezluna@gmail.com>
2019-10-25 17:57:15 -0300

    asterisco

--- src/main/java/ar/com/ada/api/inmobiliaria/entities/Inmueble.java
+++ src/main/java/ar/com/ada/api/inmobiliaria/entities/Inmueble.java
@@ -3,17 +3,7 @@ package ar.com.ada.api.inmobiliaria.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*; 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
